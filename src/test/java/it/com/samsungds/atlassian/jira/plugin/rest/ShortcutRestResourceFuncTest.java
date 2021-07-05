0485a489691ea71529790871dc385182202d8633
W.Y.Park <wypark70@gmail.com>
2021-07-06 03:16:48 +0900

    https 설정 추가

--- src/test/java/it/com/samsungds/atlassian/jira/plugin/rest/ShortcutRestResourceFuncTest.java
+++ src/test/java/it/com/samsungds/atlassian/jira/plugin/rest/ShortcutRestResourceFuncTest.java
@@ -1,39 +1,38 @@
 package it.com.samsungds.atlassian.jira.plugin.rest;
 
-import org.junit.Test;
-import org.junit.After;
-import org.junit.Before;
-import org.mockito.Mockito;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
-import com.samsungds.atlassian.jira.plugin.rest.ShortcutRestResource;
 import com.samsungds.atlassian.jira.plugin.rest.ShortcutRestResourceModel;
 import org.apache.wink.client.Resource;
 import org.apache.wink.client.RestClient;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class ShortcutRestResourceFuncTest {
 
-  @Before
-  public void setup() {
+    @Before
+    public void setup() {
 
-  }
+    }
 
-  @After
-  public void tearDown() {
+    @After
+    public void tearDown() {
 
-  }
+    }
 
-  @Test
-  public void messageIsValid() {
+    @SuppressWarnings("SpellCheckingInspection")
+    @Test
+    public void messageIsValid() {
 
-    String baseUrl = System.getProperty("baseurl");
-    String resourceUrl = baseUrl + "/rest/shortcutrestresource/1.0/shortcut/message";
+        String baseUrl = System.getProperty("baseurl");
+        String resourceUrl = baseUrl + "/rest/shortcutrestresource/1.0/shortcut/message";
 
-    RestClient client = new RestClient();
-    Resource resource = client.resource(resourceUrl);
+        RestClient client = new RestClient();
+        Resource resource = client.resource(resourceUrl);
 
-    ShortcutRestResourceModel message = resource.get(ShortcutRestResourceModel.class);
+        ShortcutRestResourceModel message = resource.get(ShortcutRestResourceModel.class);
 
-    assertEquals("wrong message", "Hello World", message.getMessage());
-  }
+        assertEquals("wrong message", "Hello World", message.getMessage());
+    }
 }
